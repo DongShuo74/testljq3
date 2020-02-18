cc521118641a18620c7efeca56c3b68a47eb367c
DongShuo <819351308@qq.com>
2020-02-18 11:36:48 +0800

    Initial commit

---
+++ src/main/java/com/testgit3/App.java
@@ -0,0 +1,15 @@
+package com.testgit3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println("ceshigit");
+        System.out.println( "Hello World!" );
+    }
+}
